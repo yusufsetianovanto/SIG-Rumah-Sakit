3bfbb36df18ca92403df01970838d501a542522b
dieonly <yusufsetya77@gmail.com>
2021-07-25 20:17:47 +0800

    Done

---
+++ about.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>About</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .googlemap {
+            align-content: center;
+
+        }
+
+        .list {
+            padding-left: 50px;
+        }
+
+        img {
+            height: 150px;
+        }
+
+        .jumbotron{
+            background-color: #DAA520;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="text-center" style="margin-bottom:0">
+        <img src="img/Logo.png" alt="">
+        <p>
+        <h1>Pendataan Rumah Sakit - Daerah Banjarmasin</h1>
+        </p>
+    </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="data.php">Data</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container" style="margin-top:30px">
+        <p>Dalam sejarah kuno, kepercayaan dan pengobatan berhubungan sangat erat. Salah satu contoh institusi
+            pengobatan tertua
+            adalah kuil Mesir. Kuil Asclepius di Yunani juga dipercaya memberikan pengobatan kepada orang sakit, yang
+            kemudian juga
+            diadopsi bangsa Romawi sebagai kepercayaan. Kuil Romawi untuk Æsculapius dibangun pada tahun 291 SM di tanah
+            Tiber, Roma
+            dengan ritus-ritus hampir sama dengan kepercayaan Yunani.</p>
+        <p>Institusi yang spesifik untuk pengobatan pertama kali, ditemukan di India. Rumah sakit Brahmanti pertama kali
+            didirikan
+            di Sri Lanka pada tahun 431 SM, kemudian Raja Ashoka juga mendirikan 18 rumah sakit di Hindustan pada 230 SM
+            dengan
+            dilengkapi tenaga medis dan perawat yang dibiayai anggaran kerajaan.</p>
+        <p>Rumah sakit pertama yang melibatkan pula konsep pengajaran pengobatan, dengan mahasiswa yang diberikan
+            pengajaran oleh
+            tenaga ahli, adalah Akademi Gundishapur di Kerajaan Persia.</p>
+        <p>Bangsa Romawi menciptakan valetudinaria untuk pengobatan budak, gladiator, dan prajurit sekitar 100 SM.
+            Adopsi
+            kepercayaan Kristiani turut memengaruhi pelayanan medis di sana. Konsili Nicea I pada tahun 325
+            memerintahkan pihak
+            Gereja untuk juga memberikan pelayanan kepada orang-orang miskin, sakit, janda, dan musafir. Setiap satu
+            katedral di
+            setiap kota harus menyediakan satu pelayanan kesehatan. Salah satu yang pertama kali mendirikan adalah Saint
+            Sampson di
+            Konstantinopel dan Basil, bishop of Caesarea. Bangunan ini berhubungan langsung dengan bagunan gereja, dan
+            disediakan
+            pula tempat terpisah untuk penderita lepra.</p>
+        <p>Rumah sakit abad pertengahan di Eropa juga mengikuti pola tersebut. Di setiap tempat peribadahan biasanya
+            terdapat
+            pelayanan kesehatan oleh pendeta dan suster (Frase Perancis untuk rumah sakit adalah hôtel-Dieu, yang
+            berarti “hostel of
+            God.”). Namun beberapa di antaranya bisa pula terpisah dari tempat peribadahan. Ditemukan pula rumah sakit
+            yang
+            terspesialisasi untuk penderita lepra, kaum miskin, atau musafir.</p>
+        <p>Rumah sakit dalam sejarah Islam memperkenalkan standar pengobatan yang tinggi pada abad 8 hingga 12. Rumah
+            sakit pertama
+            dibangun pada abad 9 hingga 10 mempekerjakan 25 staff pengobatan dan perlakuan pengobatan berbeda untuk
+            penyakit yang
+            berbeda pula. Rumah sakit yang didanai pemerintah muncul pula dalam sejarah Tiongkok pada awal abad 10.</p>
+        <p>Perubahan rumah sakit menjadi lebih sekuler di Eropa terjadi pada abad 16 hingga 17. Tetapi baru pada abad 18
+            rumah
+            sakit modern pertama dibangun dengan hanya menyediakan pelayanan dan pembedahan medis. Inggris pertama kali
+            memperkenalkan konsep ini. Guy’s Hospital didirikan di London pada 1724 atas permintaan seorang saudagar
+            kaya Thomas
+            Guy. Rumah sakit yang dibiayai swasta seperti ini kemudian menjamur di seluruh Inggris Raya. Di koloni
+            Inggris di
+            Amerika kemudian berdiri Pennsylvania General Hospital di Philadelphia pada 1751. Setelah terkumpul
+            sumbangan £2,000, di
+            Eropa Daratan biasanya rumah sakit dibiayai dana publik. Namun secara umum pada pertengahan abad 19 hampir
+            seluruh
+            negara di Eropa dan Amerika Utara telah memiliki keberagaman rumah sakit.</p>
+    </div>
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <p><b>@Kelompok 1</b></p>
+    </div>
+
+</body>
+
+</html>
